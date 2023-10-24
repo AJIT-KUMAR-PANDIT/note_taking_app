6de54c12110a10c1c211b0dced2d560118cd4e38
AJIT KUMAR PANDIT <ajitkumarpandit1999@gmail.com>
2023-10-24 16:08:56 +0530

    V1,1 MOBILE COMPATIABALITY

--- src/Components/LeftSidePannel/LeftSidePannel.jsx
+++ src/Components/LeftSidePannel/LeftSidePannel.jsx
@@ -4,6 +4,9 @@ const LeftSidePannel = () => {
     return (
         <div className={StylesLeftSidePannel.leftSidePannel}>
             <h1>Pocket Notes</h1>
+            <div>
+                <button className={StylesLeftSidePannel.createNotesGroup}> + Create Notes group</button>
+            </div>
         </div>
     );
 }
